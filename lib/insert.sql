852999662ae4997014555068bb30b6d5e9e95b95
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-10-27 16:42:23 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 19, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 21, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 15, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 12, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 7, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
